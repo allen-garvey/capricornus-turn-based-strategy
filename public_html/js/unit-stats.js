5ec07c7c0b450cc0874dfa280b9b62744872006a
Allen Garvey <allen.garvey@gmail.com>
2017-04-23 18:02:53 -0400

    Add other units to unit stats

--- public_html/js/unit-stats.js
+++ public_html/js/unit-stats.js
@@ -10,13 +10,34 @@ app.unitStats = (function(){
     function unitStats(){
     	var stats = [];
     	stats.push({
-    		name: 'Soldier', //for debugging purposes
+    		name: 'Infantry', //for debugging purposes
     		spritesheet: soldierSpriteSheet,
     		spriteCoordinate: {x: 0, y: 1},
     		canTraverse: [true, true], //if a unit can traverse the type of terrain cross-referenced to terrainStats array index
+    		applyDefense: true, //gets defense from cover
+    		hitpoints: 75, //hitpoints contained in new unit
+    		attackTable: [20, 20, 20], //damage inflicted to a unit of the same type as the array index
+    		movementSpeed: 6 //how many tiles unit can move
+    	});
+    	stats.push({
+    		name: 'Tank', //for debugging purposes
+    		spritesheet: soldierSpriteSheet,
+    		spriteCoordinate: {x: 0, y: 1},
+    		canTraverse: [true, true], //if a unit can traverse the type of terrain cross-referenced to terrainStats array index
+    		applyDefense: false, //gets defense from cover
+    		hitpoints: 150, //hitpoints contained in new unit
+    		attackTable: [50, 50, 50], //damage inflicted to a unit of the same type as the array index
+    		movementSpeed: 4 //how many tiles unit can move
+    	});
+    	stats.push({
+    		name: 'Plane', //for debugging purposes
+    		spritesheet: soldierSpriteSheet,
+    		spriteCoordinate: {x: 0, y: 1},
+    		canTraverse: [true, true], //if a unit can traverse the type of terrain cross-referenced to terrainStats array index
+    		applyDefense: false, //gets defense from cover
     		hitpoints: 100, //hitpoints contained in new unit
-    		attackTable: [50], //damage inflicted to a unit of the same type as the array index
-    		movementSpeed: 5 //how many tiles unit can move
+    		attackTable: [30, 30, 30], //damage inflicted to a unit of the same type as the array index
+    		movementSpeed: 8 //how many tiles unit can move
     	});
     	return stats;
     }
