97bb41792ac8ab48540e2a1a8393ac757341c4e7
Allen Garvey <allen.garvey@gmail.com>
2017-04-23 17:44:41 -0400

    Create module for unit pathfinding and stub method for getting movement coordinates

---
+++ public_html/js/pathfinding.js
@@ -0,0 +1,37 @@
+/*
+ * Logic for unit pathfinding
+ */
+var app = app || {};
+app.pathfinder = (function(){
+
+	/*
+	* @param unitCoordinate - coordinate {x, y} of the unit
+	* @param gamboard - 2 dimensional array of units and terrain
+	* @param - unitStatsArray - array of unit stats, cross-indexed to unit.type
+	* @param - terrainStatsArray - array of terrain stats, cross-indexed to terrain.type
+	* @returns array of coordinates (in any order) that the unit at unitCoordinate can travel to (not including starting unitCoordinate)
+	(should also probably return coordinates of units that can be attacked, with additional property to coordinate, such as {x: 1, y: 1, attack: true})
+	*/
+    function movementCoordinatesFor(unitCoordinate, gameboard, unitStatsArray, terrainStatsArray){
+    	//get the unit
+    	var unitToBeMoved = gameboard[unitCoordinate.x][unitCoordinate.y].unit;
+    	//get stats about the unit
+    	var unitStats = unitStatsArray[unitToBeMoved.type];
+
+    	//example of getting terrain at coordinate {x: 1, y: 2}
+    	var terrain = gameboard[1][2].terrain;
+    	//see if unit can traverse that terrain
+    	if(unitStats.canTraverse[terrain.type]){
+
+    	}
+
+    	//for now just return squares around the unit
+    	return [{x: unitCoordinate.x + 1, y: unitCoordinate.y}, {x: unitCoordinate.x - 1, y: unitCoordinate.y}, {x: unitCoordinate.x, y: unitCoordinate.y + 1}, {x: unitCoordinate.x, y: unitCoordinate.y - 1}];
+    }
+
+
+    //exported functions
+    return {
+    	movementCoordinatesFor: movementCoordinatesFor
+    };
+})();
