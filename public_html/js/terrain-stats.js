5189e4701486774735da75f7e03964e805ee26de
Allen Garvey <allen.garvey@gmail.com>
2017-04-23 17:45:09 -0400

    Add documentation to terrain stats and add constructor for terrain instance

--- public_html/js/terrain-stats.js
+++ public_html/js/terrain-stats.js
@@ -5,21 +5,35 @@ var app = app || {};
 
 app.terrainStats = (function(){
 	var terrainSpritesheet = document.getElementById('spritesheet');
+    
+    //array of information about terrain - corresponds to terrain instance type
     function terrainStats(){
     	var stats = [];
     	stats.push({
     		name: 'Sand',
     		spritesheet: terrainSpritesheet,
-    		spriteCoordinate: {x: 0, y: 0}
+    		spriteCoordinate: {x: 0, y: 0},
+            defense: 1
     	});
         stats.push({
             name: 'Grass',
             spritesheet: terrainSpritesheet,
-            spriteCoordinate: {x: 1, y: 0}
+            spriteCoordinate: {x: 1, y: 0},
+            defense: 2
         });
     	return stats;
     }
 
+    //returns new terrain instance
+    function terrain(type){
+        return {
+            type: type //array index in terrainStas
+        };
+    }
+
     //exported functions
-    return {get: terrainStats};
+    return {
+            get: terrainStats,
+            create: terrain
+            };
 })();
