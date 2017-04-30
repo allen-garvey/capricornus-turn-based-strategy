b3382c3efddeaa9a39f97857e587441acf0e9cbf
Allen Garvey <allen.garvey@gmail.com>
2017-04-29 23:26:21 -0400

    Initial implementation of rendering unit movement animation

--- public_html/js/renderer.js
+++ public_html/js/renderer.js
@@ -5,7 +5,7 @@
  */
 var app = app || {};
 
-app.renderer = (function(unitStats, terrainStats){
+app.renderer = (function(util, unitStats, terrainStats){
 	var UNIT_STATS = unitStats.get();
 	var TERRAIN_STATS = terrainStats.get();
 	
@@ -14,6 +14,10 @@ app.renderer = (function(unitStats, terrainStats){
 	*/
 	//pixels in square tile
 	var TILE_SIZE = 32;
+	//number of pixels between each frame in the animation
+	var ANIMATION_PIXEL_STEP = 1;
+	//delay between frames in ms
+	var ANIMATION_FRAME_DELAY = 10;
 
 	function totalTiles(canvasParent){
 		return {x: Math.floor(canvasParent.offsetWidth / TILE_SIZE), y: Math.floor(canvasParent.offsetHeight / TILE_SIZE)};
@@ -52,15 +56,27 @@ app.renderer = (function(unitStats, terrainStats){
 	/*
 	* General drawing and erasing functions
 	*/
-	function drawTile(canvasContext, image, locationCoordinate, spriteCoordinate){
-		var pixelLocationCoordinate = tileCoordinateToPixelCoordinate(locationCoordinate);
+	function drawTileAtPixelCoordinate(canvasContext, image, pixelLocationCoordinate, spriteCoordinate){
 		var spritePixelCoordinate = tileCoordinateToPixelCoordinate(spriteCoordinate);
 		canvasContext.drawImage(image, spritePixelCoordinate.x, spritePixelCoordinate.y, TILE_SIZE, TILE_SIZE, pixelLocationCoordinate.x, pixelLocationCoordinate.y, TILE_SIZE, TILE_SIZE);
 	}
 
+
+	function drawTile(canvasContext, image, locationCoordinate, spriteCoordinate){
+		var pixelLocationCoordinate = tileCoordinateToPixelCoordinate(locationCoordinate);
+		// var spritePixelCoordinate = tileCoordinateToPixelCoordinate(spriteCoordinate);
+		// canvasContext.drawImage(image, spritePixelCoordinate.x, spritePixelCoordinate.y, TILE_SIZE, TILE_SIZE, pixelLocationCoordinate.x, pixelLocationCoordinate.y, TILE_SIZE, TILE_SIZE);
+		drawTileAtPixelCoordinate(canvasContext, image, pixelLocationCoordinate, spriteCoordinate);
+	}
+
+	function eraseTileAtPixelCoordinate(canvasContext, pixelLocationCoordinate){
+		canvasContext.clearRect(pixelLocationCoordinate.x, pixelLocationCoordinate.y, TILE_SIZE, TILE_SIZE);
+	}
+
 	function eraseTile(canvasContext, locationCoordinate){
 		var pixelLocationCoordinate = tileCoordinateToPixelCoordinate(locationCoordinate);
-		canvasContext.clearRect(pixelLocationCoordinate.x, pixelLocationCoordinate.y, TILE_SIZE, TILE_SIZE);
+		eraseTileAtPixelCoordinate(canvasContext, pixelLocationCoordinate);
+		// canvasContext.clearRect(pixelLocationCoordinate.x, pixelLocationCoordinate.y, TILE_SIZE, TILE_SIZE);
 	}
 
 	function eraseCanvas(canvasContext){
@@ -74,6 +90,10 @@ app.renderer = (function(unitStats, terrainStats){
 	function renderUnit(canvasContext, coordinate, unit){
 		var unitStats = UNIT_STATS[unit.type];
 		drawTile(canvasContext, unitStats.spritesheets[unit.team], coordinate, unitStats.spriteCoordinates[unit.team][unit.currentDirection]);
+	}
+	function renderUnitAtPixelCoordinate(canvasContext, pixelCoordinate, unit){
+		var unitStats = UNIT_STATS[unit.type];
+		drawTileAtPixelCoordinate(canvasContext, unitStats.spritesheets[unit.team], pixelCoordinate, unitStats.spriteCoordinates[unit.team][unit.currentDirection]);
 	}
 
 	/*
@@ -114,6 +134,106 @@ app.renderer = (function(unitStats, terrainStats){
 		canvasContext.fill();
 	}
 
+	/**
+	 * Rending unit movement
+	 */
+	 //displays animation showing unit moving between 2 adjacent tiles
+	 //based on: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+	 function renderUnitMovingBetween(animationCanvasContext, unit, startingCoordinate, endingCoordinate, nextCallback){
+	 	var DIRECTIONS = {
+	 		RIGHT: 0,
+	 		LEFT: 1,
+	 		UP: 2,
+	 		DOWN: 3
+	 	};
+	 	var start = null;
+	 	var startingPixelCoordinate = tileCoordinateToPixelCoordinate(startingCoordinate);
+	 	var endingPixelCoordinate = tileCoordinateToPixelCoordinate(endingCoordinate);
+	 	var currentPixelCoordinate = util.copyCoordinate(startingPixelCoordinate);
+	 	var currentDirection = DIRECTIONS.RIGHT;
+	 	if(endingCoordinate.x < startingCoordinate.x){
+	 		currentDirection = DIRECTIONS.LEFT;
+	 	}
+	 	else if(endingCoordinate.y > startingCoordinate.y){
+	 		currentDirection = DIRECTIONS.DOWN;
+	 	}
+	 	else if(endingCoordinate.y < startingCoordinate.y){
+	 		currentDirection = DIRECTIONS.UP;
+	 	}
+	 	function step(timestamp){
+			if(start === null){
+				start = timestamp;	
+			}
+			var progress = timestamp - start;
+			//don't show next frame until certain amount of time has passed
+			if(progress < ANIMATION_FRAME_DELAY){
+		    	window.requestAnimationFrame(step);
+		    	return;
+			}
+			//erase previous unit position
+			eraseTileAtPixelCoordinate(animationCanvasContext, currentPixelCoordinate);
+			//update current position
+			switch(currentDirection){
+				case DIRECTIONS.UP:
+					currentPixelCoordinate.y -= ANIMATION_PIXEL_STEP;
+					break;
+				case DIRECTIONS.LEFT:
+					currentPixelCoordinate.x -= ANIMATION_PIXEL_STEP;
+					break;
+				case DIRECTIONS.RIGHT:
+					currentPixelCoordinate.x += ANIMATION_PIXEL_STEP;
+					break;
+				default:
+					currentPixelCoordinate.y += ANIMATION_PIXEL_STEP;
+					break;
+			}
+			//render unit at current position
+			renderUnitAtPixelCoordinate(animationCanvasContext, currentPixelCoordinate, unit);
+			if(!util.areCoordinatesEqual(currentPixelCoordinate, endingPixelCoordinate)){
+				//reset start
+				start = null;
+				window.requestAnimationFrame(step);
+			}
+			else{
+				nextCallback();
+			}
+		}
+	 	window.requestAnimationFrame(step);
+	 }
+
+	 function renderUnitMovement(unitCanvasContext, animationCanvasContext, unit, path, resolveCallback){
+	 	var startingCoordinate = path[0];
+	 	//draw unit initial position on animation canvas, and erase unit on unit canvas
+	 	renderUnit(animationCanvasContext, startingCoordinate, unit);
+	 	eraseTile(unitCanvasContext, startingCoordinate);
+	 	
+	 	//to be executed on function completion
+	 	function endingCallback(){
+	 		//draw ending unit position on unit canvas, and erase unit on animation canvas
+		 	var endingCoordinate = path[path.length - 1];
+		 	renderUnit(unitCanvasContext, endingCoordinate, unit);
+		 	eraseTile(animationCanvasContext, endingCoordinate);
+		 	resolveCallback();
+	 	};
+	 	var i = 1;
+	 	function nextAnimationCallback(){
+	 		i++;
+	 		if(i < path.length - 1){
+ 				renderUnitMovingBetween(animationCanvasContext, unit, path[i], path[i+1], nextAnimationCallback);
+ 			}
+ 			else{
+ 				endingCallback();
+ 			}
+
+	 	};
+	 	if(path.length > 1){
+	 		renderUnitMovingBetween(animationCanvasContext, unit, path[0], path[1], nextAnimationCallback);
+	 	}
+	 	else{
+	 		endingCallback();
+	 	}
+	 }
+
 	//exported functions and variables
 	return {
 		TILE_SIZE: TILE_SIZE,
@@ -129,6 +249,7 @@ app.renderer = (function(unitStats, terrainStats){
 		gameTileForCoordinate: gameTileForCoordinate,
 		renderUnitSelectionOutline: renderUnitSelectionOutline,
 		renderUnitMovementSquares: renderUnitMovementSquares,
-		renderLevel: renderLevel
+		renderLevel: renderLevel,
+		renderUnitMovement: renderUnitMovement
 	};
-})(app.unitStats, app.terrainStats);
+})(app.util, app.unitStats, app.terrainStats);
