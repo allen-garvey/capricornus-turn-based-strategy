cf94dc6bdfc432f96bc72ea025a2a7481d2856f9
Allen Garvey <allen.garvey@gmail.com>
2017-04-29 23:25:02 -0400

    Create various utility functions

---
+++ public_html/js/util.js
@@ -0,0 +1,32 @@
+/*
+ * Utility functions
+ */
+var app = app || {};
+
+app.util = (function(){
+	function copyCoordinate(coordinate){
+		return {x: coordinate.x, y: coordinate.y};
+	}
+
+	function isCoordinateInMovementSquares(coordinate, movementSquares){
+		for(var i = 0; i < movementSquares.length; i++){
+			var currentCoordinate = movementSquares[i];
+			if(coordinate.x === currentCoordinate.x && coordinate.y === currentCoordinate.y){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function areCoordinatesEqual(coordinate1, coordinate2){
+		return (coordinate1.x === coordinate2.x && coordinate1.y === coordinate2.y);
+	}
+
+	//exported functions and variables
+	return {
+		copyCoordinate: copyCoordinate,
+		isCoordinateInMovementSquares: isCoordinateInMovementSquares,
+		areCoordinatesEqual: areCoordinatesEqual
+	};
+    
+})();
