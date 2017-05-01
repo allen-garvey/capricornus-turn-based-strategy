9b399c6f371946f66778149c10ce99c1efe7558d
livermok <kyle.livermore@hotmail.com>
2017-04-30 17:21:56 -0700

    Fixed a bug in valid move finding algorithim and added pathfinding

--- public_html/js/pathfinding.js
+++ public_html/js/pathfinding.js
@@ -19,10 +19,33 @@ app.pathfinder = (function(util){
 		var unitToBeMoved = gameboard[startingCoordinate.x][startingCoordinate.y].unit;
 		//get stats about the unit
     	var unitStats = unitStatsArray[unitToBeMoved.type];
-
+		var validMoves = movementCoordinatesFor(startingCoordinate, gameboard, unitStatsArray, terrainStatsArray);
+		var pathReverse = [];
+		var path = [];
+	
+		if(arrayContainsCoords(validMoves, endingCoordinate.x, endingCoordinate.y)){
+			pathReverse.push(endingCoordinate);
+			while (pathReverse[pathReverse.length - 1].x != startingCoordinate.x || pathReverse[pathReverse.length - 1].y != startingCoordinate.y )
+			{
+				var coordinate = getCoordinateFromMoveArray(validMoves, pathReverse[pathReverse.length - 1].x, pathReverse[pathReverse.length - 1].y);
+				pathReverse.push({x: coordinate.fromX, y: coordinate.fromY});
+			}
+		}
+		else
+		{
+			//This is the error case where the ending coordinate is not in the array of valid moves
+			path.push({x: startingCoordinate.x, y: startingCoordinate.y + 1});
+		}
+		
+		for (var ixx = pathReverse.length - 1; ixx >= 0; ixx--)
+		{
+			path.push(pathReverse[ixx]);
+		}
+		//document.getElementById("testOut").textContent = JSON.stringify(path);
+    	return path;
     	//TODO: Add code for shortest path between starting and ending coordinate here
     	//right now this is a stub algorithm for testing purposes
-    	var coordinatesPath = [startingCoordinate];
+    	/* var coordinatesPath = [startingCoordinate];
     	var currentCoordinate = {x: startingCoordinate.x, y: startingCoordinate.y};
     	while(!util.areCoordinatesEqual(currentCoordinate, endingCoordinate)){
     		if(currentCoordinate.x < endingCoordinate.x){
@@ -40,7 +63,7 @@ app.pathfinder = (function(util){
     		coordinatesPath.push(util.copyCoordinate(currentCoordinate));
     	}
 
-    	return coordinatesPath;
+    	return coordinatesPath; */
 
 	}
 
@@ -72,22 +95,22 @@ app.pathfinder = (function(util){
 		if(unitCoordinate.x + 1 <= xmax 
 		&& unitStats.canTraverse[gameboard[unitCoordinate.x + 1][unitCoordinate.y].terrain.type]
 		&& gameboard[unitCoordinate.x + 1][unitCoordinate.y].unit === null ){
-			validMoves.push({x: unitCoordinate.x + 1, y: unitCoordinate.y, cost: 1});	
+			validMoves.push({x: unitCoordinate.x + 1, y: unitCoordinate.y, cost: 1, fromX: unitCoordinate.x, fromY: unitCoordinate.y});	
 		}
 		if(unitCoordinate.x - 1 >= 0 
 		&& unitStats.canTraverse[gameboard[unitCoordinate.x - 1][unitCoordinate.y].terrain.type]
 		&& gameboard[unitCoordinate.x - 1][unitCoordinate.y].unit === null ){
-			validMoves.push({x: unitCoordinate.x - 1, y: unitCoordinate.y, cost: 1});
+			validMoves.push({x: unitCoordinate.x - 1, y: unitCoordinate.y, cost: 1, fromX: unitCoordinate.x, fromY: unitCoordinate.y});
 		}
 		if(unitCoordinate.y + 1 <= ymax 
 		&& unitStats.canTraverse[gameboard[unitCoordinate.x][unitCoordinate.y + 1].terrain.type]
 		&& gameboard[unitCoordinate.x][unitCoordinate.y + 1].unit === null ){
-			validMoves.push({x: unitCoordinate.x, y: unitCoordinate.y + 1, cost: 1});
+			validMoves.push({x: unitCoordinate.x, y: unitCoordinate.y + 1, cost: 1, fromX: unitCoordinate.x, fromY: unitCoordinate.y});
 		}
 		if(unitCoordinate.y - 1 >= 0 
 		&& unitStats.canTraverse[gameboard[unitCoordinate.x][unitCoordinate.y - 1].terrain.type]
 		&& gameboard[unitCoordinate.x][unitCoordinate.y - 1].unit === null ){
-			validMoves.push({x: unitCoordinate.x, y: unitCoordinate.y - 1, cost: 1});
+			validMoves.push({x: unitCoordinate.x, y: unitCoordinate.y - 1, cost: 1, fromX: unitCoordinate.x, fromY: unitCoordinate.y});
 		} 
 		var nextMoveDist = 1;
 		index = 0;
@@ -98,30 +121,30 @@ app.pathfinder = (function(util){
 			&& unitStats.canTraverse[gameboard[validMoves[index].x + 1][validMoves[index].y].terrain.type]
 			&& !arrayContainsCoords(validMoves, validMoves[index].x + 1, validMoves[index].y)
 			&& gameboard[validMoves[index].x + 1][validMoves[index].y].unit === null ){
-				validMoves.push({x: validMoves[index].x + 1, y: validMoves[index].y, cost: validMoves[index].cost + 1});	
+				validMoves.push({x: validMoves[index].x + 1, y: validMoves[index].y, cost: validMoves[index].cost + 1, fromX: validMoves[index].x, fromY: validMoves[index].y});	
 			}
 			if(validMoves[index].x - 1 >= 0 
 			&& unitStats.canTraverse[gameboard[validMoves[index].x - 1][validMoves[index].y].terrain.type]
 			&& !arrayContainsCoords(validMoves, validMoves[index].x - 1, validMoves[index].y)
 			&& gameboard[validMoves[index].x - 1][validMoves[index].y].unit === null){
-				validMoves.push({x: validMoves[index].x - 1, y: validMoves[index].y, cost: validMoves[index].cost + 1});
+				validMoves.push({x: validMoves[index].x - 1, y: validMoves[index].y, cost: validMoves[index].cost + 1, fromX: validMoves[index].x, fromY: validMoves[index].y});
 			}
 			if(validMoves[index].y + 1 <= ymax 
 			&& unitStats.canTraverse[gameboard[validMoves[index].x][validMoves[index].y + 1].terrain.type]
 			&& !arrayContainsCoords(validMoves, validMoves[index].x, validMoves[index].y + 1)
 			&& gameboard[validMoves[index].x][validMoves[index].y + 1].unit === null){
-				validMoves.push({x: validMoves[index].x, y: validMoves[index].y + 1, cost: validMoves[index].cost + 1});
+				validMoves.push({x: validMoves[index].x, y: validMoves[index].y + 1, cost: validMoves[index].cost + 1, fromX: validMoves[index].x, fromY: validMoves[index].y});
 			}
 			if(validMoves[index].y - 1 >= 0 
 			&& unitStats.canTraverse[gameboard[validMoves[index].x][validMoves[index].y - 1].terrain.type]
 			&& !arrayContainsCoords(validMoves, validMoves[index].x, validMoves[index].y - 1)
 			&& gameboard[validMoves[index].x][validMoves[index].y - 1].unit === null){
-				validMoves.push({x: validMoves[index].x, y: validMoves[index].y - 1, cost: validMoves[index].cost + 1});
+				validMoves.push({x: validMoves[index].x, y: validMoves[index].y - 1, cost: validMoves[index].cost + 1, fromX: validMoves[index].x, fromY: validMoves[index].y});
 			}
 			index++;
 			nextMoveDist = validMoves[index].cost + 1;
 		}
-
+		//document.getElementById("testOut").textContent = JSON.stringify(validMoves);
     	//TODO: Add code for finding tiles unit can move to here
     	//possibly later also adding squares unit can attack
 
@@ -130,13 +153,24 @@ app.pathfinder = (function(util){
     }
 	
 	function arrayContainsCoords(movesArray, xCoordinate, yCoordinate){
-		for (var ixx = 0; ixx < movesArray.lenght; ixx++)
+		for (var ixx = 0; ixx < movesArray.length; ixx++)
 		{
 			if(movesArray[ixx].x == xCoordinate && movesArray[ixx].y == yCoordinate){
 				return true;
 			}
 		}
 		return false;
+	}
+	
+	function getCoordinateFromMoveArray(movesArray, xCoordinate, yCoordinate)
+	{
+		for (var ixx = 0; ixx < movesArray.length; ixx++)
+		{
+			if(movesArray[ixx].x == xCoordinate && movesArray[ixx].y == yCoordinate){
+				return movesArray[ixx];
+			}
+		}
+		return {x: -1, y: -1};
 	}
 
     //exported functions
