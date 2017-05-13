edc87c9957efc99bcf6ba8b13909e09cdbc65531
Allen Garvey <allen.garvey@gmail.com>
2017-05-12 23:04:29 -0400

    Fix indentation in level-stats.js

--- public_html/js/level-stats.js
+++ public_html/js/level-stats.js
@@ -11,21 +11,21 @@ app.levelStats = (function(){
     	stats.push({
     		name: 'Level 0', //for debugging purposes
     		spritesheet: document.getElementById('level1_sprite'),
-                dataFileUrl: 'data/level1sprites.json', //file to load unit sprite locations from
-                data: null //where the data will be stored from dataFileUrl when it is loaded
+            dataFileUrl: 'data/level1sprites.json', //file to load unit sprite locations from
+            data: null //where the data will be stored from dataFileUrl when it is loaded
     	});
     	stats.push({
     		name: 'Level 1', //for debugging purposes
     		spritesheet: document.getElementById('level2_sprite'),
-                dataFileUrl: 'data/level2sprites.json', //file to load unit sprite locations from
-                data: null //where the data will be stored from dataFileUrl when it is loaded
+            dataFileUrl: 'data/level2sprites.json', //file to load unit sprite locations from
+            data: null //where the data will be stored from dataFileUrl when it is loaded
     	});
     	stats.push({
     		name: 'Level 2', //for debugging purposes
     		spritesheet: document.getElementById('level3_sprite'),
     		dataFileUrl: 'data/level3sprites.json', //file to load unit sprite locations from
-                    data: null //where the data will be stored from dataFileUrl when it is loaded
-        	});
+            data: null //where the data will be stored from dataFileUrl when it is loaded
+        });
 		    
 	    
     	return stats;
