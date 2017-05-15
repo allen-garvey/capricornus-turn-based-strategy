efe97a45bc9421a0ae00243c6b1f364af06f3f7c
Allen Garvey <allen.garvey@gmail.com>
2017-05-14 22:16:48 -0400

    Add properties to level-stats to store terrain data

--- public_html/js/level-stats.js
+++ public_html/js/level-stats.js
@@ -13,19 +13,25 @@ app.levelStats = (function(){
     		name: 'Level 0', //for debugging purposes
     		spritesheet: document.getElementById('level1_sprite'),
             dataUnitsFileUrl: 'data/level1sprites.json', //file to load unit sprite locations from
-            dataUnits: null //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataUnits: null, //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataTerrainFileUrl: 'data/level1.json', //url for terrain types for level
+            dataTerrain: null //where data from dataTerrainFileUrl will be stored after it is loaded
     	});
     	stats.push({
     		name: 'Level 1', //for debugging purposes
     		spritesheet: document.getElementById('level2_sprite'),
             dataUnitsFileUrl: 'data/level2sprites.json', //file to load unit sprite locations from
-            dataUnits: null //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataUnits: null, //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataTerrainFileUrl: 'data/level2.json', //url for terrain types for level
+            dataTerrain: null //where data from dataTerrainFileUrl will be stored after it is loaded
     	});
     	stats.push({
     		name: 'Level 2', //for debugging purposes
     		spritesheet: document.getElementById('level3_sprite'),
     		dataUnitsFileUrl: 'data/level3sprites.json', //file to load unit sprite locations from
-            dataUnits: null //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataUnits: null, //where the data will be stored from dataUnitsFileUrl when it is loaded
+            dataTerrainFileUrl: 'data/level2.json', //url for terrain types for level
+            dataTerrain: null //where data from dataTerrainFileUrl will be stored after it is loaded
         });
 		    
 	    
