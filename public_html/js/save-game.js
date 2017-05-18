bc0fcdaa7e59af1d005e499d038791c68d32227c
griffste <stevegriffin86@gmail.com>
2017-05-17 20:00:34 -0500

    Filled in functions for save-game.js

--- public_html/js/save-game.js
+++ public_html/js/save-game.js
@@ -17,8 +17,61 @@ app.saveGame = (function(){
 	 * can be queried
 	 */
 	function createSave(name, gameboard, gameMetadata){
-		return false;
+		
+		//Get timestamp for save
+		var date = new Date();
+		
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		if (month < 10)
+			month = '0' + month;
+		var day = date.getDate();
+		if (day < 10)
+			day = '0' + day;
+		var hours = date.getHours();
+		if(hours < 10)
+			hours = '0' + hours;
+		var minutes = date.getMinutes();
+		if(minutes < 10)
+			minutes = '0' + minutes;
+		var seconds = date.getSeconds();
+		if(seconds < 10)
+			seconds = '0' + seconds;
+		
+		var timestamp = year + "/" + month + "/" + day + " " + hours + ":" + minutes + ":" + seconds;
+
+		//Give unique ID
+		var id;
+		if(localStorage.length == 0) {
+			id = 'capricornus0';
+		}
+		else {
+			var data = JSON.parse(localStorage.getItem(localStorage.key(localStorage.length-1)));
+			var prevId = data['id'].substring(11);
+			var num = Number(prevId);
+			num++;    
+			id = 'capricornus' + num; 
+		}
+	
+		//Assign variables
+		var state = {
+			id: id,
+			name : name,
+			gameboard : gameboard,
+			gameMetadata : gameMetadata,
+			formattedDate : timestamp
+		};
+
+		//Save data in localStorage
+		try {
+			localStorage.setItem(id, JSON.stringify(state));
+			return true;
+		}
+		catch(exception) {
+			return false;
+		}
 	}
+	
 
 	/*
 	 * Deletes a saved game from browser localStorage
@@ -27,9 +80,21 @@ app.saveGame = (function(){
 	 * id was not found, or the delete action failed
 	 */
 	function deleteSave(savegameId){
+		
+		if(localStorage.getItem(savegameId) != null)
+		{
+			try {
+				localStorage.removeItem(savegameId);
+				return true;
+			}
+			catch(exception) {
+				return false;
+			}
+		}
 		return false;
-	}
+	}	
 
+	
 	/*
 	 * Used to load a saved game from browser localStorage
 	 * @param savegameId - either int or string - whatever datatype is returned from getSaves()
@@ -43,6 +108,8 @@ app.saveGame = (function(){
 	 	}
 	 */
 	function getSave(savegameId){
+		if(localStorage.getItem(savegameId) != null)
+			return JSON.parse(localStorage.getItem(savegameId));
 		return null;
 	}
 
@@ -62,7 +129,28 @@ app.saveGame = (function(){
 	 	array should be sorted in reverse order based on datetime of saved game creation, so that the newer saved games come before older saved games
 	 */
 	function getSaves(){
-		return null;
+		var saveArr = [];
+		for(var i = 0; i < localStorage.length; i++)
+		{
+			var data = JSON.parse(localStorage.getItem(localStorage.key(i)));
+				saveArr.push({
+					id: data['id'],
+					name: data['name'],
+					formattedDate: data['formattedDate'],
+					gameMetadata: data['gameMetadata']
+				});
+		}
+		
+		//Sorts by formatted date, with most recently saved first 
+		saveArr.sort(function(a,b) {
+			return (a.formattedDate < b.formattedDate) - (a.formattedDate > b.formattedDate);
+		});
+		
+				
+		if(saveArr.length > 0)
+			return saveArr;
+		else
+			return null;
 	}
 
 	//exported functions and variables
