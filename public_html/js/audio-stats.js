7a6ddd87f715883ce9eb3fa79dc618612c768a6e
Allen Garvey <allen.garvey@gmail.com>
2017-05-20 23:41:14 -0400

    Create modules to play audio and keep track of audio files

---
+++ public_html/js/audio-stats.js
@@ -0,0 +1,57 @@
+"use strict";
+/*
+ * Stores information about audio sound effects
+ */
+var app = app || {};
+
+app.audioStats = (function(){
+
+    
+	//array of information about sound effects for units - cross referenced to unit stats array
+    function audioStats(){
+        var stats = {
+            units: []
+        };
+
+    	stats.units.push({
+    		name: 'Infantry', //for debugging purposes
+    		attackUrl: 'sounds/soldierAttack.ogg', //url for sound effect audio file used when unit attacks 
+            moveUrl: 'sounds/soldierMove.ogg', //url for sound effect audio file used when unit moves
+            damageUrl: '', //url for sound effect audio file used when unit takes damage
+            dieUrl: 'sounds/death.ogg', //url for sound effect audio file used when unit dies
+            move: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            attack: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            damage: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            die: null //AudioBuffer object is placed here once corresponding audio file is preloaded
+    	});
+    	stats.units.push({
+    		name: 'Tank', //for debugging purposes
+    		attackUrl: 'sounds/tankAttack.ogg', //url for sound effect audio file used when unit attacks 
+            moveUrl: 'sounds/tankMove.ogg', //url for sound effect audio file used when unit moves
+            damageUrl: '', //url for sound effect audio file used when unit takes damage
+            dieUrl: 'sounds/death.ogg', //url for sound effect audio file used when unit dies
+            move: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            attack: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            damage: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            die: null //AudioBuffer object is placed here once corresponding audio file is preloaded
+    	});
+    	stats.units.push({
+    		name: 'Plane', //for debugging purposes
+    		attackUrl: 'sounds/planeAttack.ogg', //url for sound effect audio file used when unit attacks 
+            moveUrl: 'sounds/planeMove.ogg', //url for sound effect audio file used when unit moves
+            damageUrl: '', //url for sound effect audio file used when unit takes damage
+            dieUrl: 'sounds/death.ogg', //url for sound effect audio file used when unit dies
+            move: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            attack: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            damage: null, //AudioBuffer object is placed here once corresponding audio file is preloaded
+            die: null //AudioBuffer object is placed here once corresponding audio file is preloaded
+    	});
+    	return stats;
+    }
+
+
+    //exported functions
+    return {
+    		get: audioStats
+    		};
+})();
---
+++ public_html/js/mixer.js
@@ -0,0 +1,94 @@
+"use strict";
+/*
+ * Functions loading and playing sounds from audio files
+ */
+var app = app || {};
+
+app.mixer = (function(){
+	// Fix up prefixing
+	var AudioContext = window.AudioContext || window.webkitAudioContext;
+	var context = new AudioContext();
+
+	/**
+	 * asynchronously downloads an audio file at url and decodes into audio buffer
+	 * based on: https://www.html5rocks.com/en/tutorials/webaudio/intro/
+	 * @param url - string - url of audio file
+	 * @param callback - function - called when audio file is downloaded and AudioBuffer is decoded - passed into callback as argument 
+	 */
+	function getAudioBuffer(url, callback){
+		var request = new XMLHttpRequest();
+		request.open('GET', url, true);
+		request.responseType = 'arraybuffer';
+
+		// Decode asynchronously
+		request.onload = function() {
+			context.decodeAudioData(request.response, function(buffer){
+				callback(buffer);
+			});
+		}
+		request.send();
+	}
+
+	/**
+	 * plays sound stored in audio buffer using web audio api
+	 * based on: https://www.html5rocks.com/en/tutorials/webaudio/intro/
+	 * @param audioBuffer - AudioBuffer object preloaded with audio
+	 * @param shouldLoop - boolean - if audio should loop
+	 * @returns GainNode - use stop method to stop sound
+	 */
+	function playAudioBuffer(audioBuffer, shouldLoop){
+		var source = context.createBufferSource();    // creates a sound source
+		source.buffer = audioBuffer;                    // tell the source which sound to play
+		if(shouldLoop){
+			source.loop = true;
+		}
+		//so we can fade the sound
+		var gainNode = context.createGain();
+		source.connect(gainNode);
+		gainNode.connect(context.destination);         // connect the source to the context's destination (the speakers)
+		source.start(0);  
+		return gainNode;
+	}
+
+	/**
+	 * Stops sound started using playAudioBuffer
+	 * @param gainNode - GainNode - return value from playAudioBuffer
+	 * @param fadeOutTime - time in milliseconds to fade out sound
+	 */
+	function stopSound(gainNode, fadeOutTime){
+		if(!fadeOutTime){
+			gainNode.disconnect();
+			return;
+		}
+		var stepTime = 10; //number of milliseconds between each fade step
+		var gainStep = 100 / (fadeOutTime / stepTime) / 100;
+
+		var numberOfFades = (fadeOutTime / stepTime) - 1;
+		gainNode.gain.value = gainNode.gain.value - gainStep;
+
+		function fade(){
+			setTimeout(function(){
+				gainNode.gain.value = gainNode.gain.value - gainStep;
+				if(gainNode.gain.value > 0){
+					fade();
+				}
+				else{
+					gainNode.disconnect();
+				}
+
+			}, stepTime);
+		}
+
+		fade();
+
+		
+	}
+
+	//exported functions and variables
+	return {
+		getAudioBuffer: getAudioBuffer,
+		playAudioBuffer: playAudioBuffer,
+		stopSound: stopSound
+	};
+    
+})();
