31dbfba4c58c6a9277859f231e0a87da8661c92e
Allen Garvey <allen.garvey@gmail.com>
2017-05-27 00:45:21 -0400

    Add animation for unit damage

--- public_html/js/animation-stats.js
+++ public_html/js/animation-stats.js
@@ -12,8 +12,15 @@ app.animationStats = (function(){
     	
         stats.explosion = {
             name: 'Explosion', //for debugging purposes
+            type: 'static animation', //self-documentation about what function to use render this animation
             spritesheet: document.getElementById('explosion_spritesheet'),
             spriteCoordinates: [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 7, y: 0}, {x: 8, y: 0}, {x: 9, y: 0}, {x: 10, y: 0}, {x: 11, y: 0}] //coordinates in order animation should be displayed
+        };
+
+        stats.unitDamage = {
+            name: 'Unit damage',
+            type: 'static color animation', //self-documentation about what function to use render this animation
+            colorCells: ['rgba(255,0,0, 0.1)', 'rgba(255,0,0, 0.3)', 'rgba(255,0,0, 0.3)', 'rgba(255,0,0, 0.5)', 'rgba(255,0,0, 0.7)', 'rgba(255,0,0, 8.0)', 'rgba(255,0,0, 0.7)', 'rgba(255,0,0, 0.5)', 'rgba(255,0,0, 0.3)', 'rgba(255,0,0, 0.3)', 'rgba(255,0,0, 0.1)'] //array of css colors for used for tile colors for animation
         };
 
     	return stats;
