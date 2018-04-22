bf0dc243a0f9e38c7ad606b72ec625041d92031e
Allen Garvey <allen.garvey@gmail.com>
2018-04-21 23:02:05 -0400

    Add gulp task to create non-minified js bundle for debugging purposes

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,15 @@ const config = require(path.join(__dirname, 'gulp-config.js'));
 * JavaScript Tasks
 */
 
+gulp.task('concatScripts', function(){
+	return gulp.src(config.js.app_files)
+		.pipe(concat(`${config.js.DIST_NAME}.js`))
+		.pipe(gulp.dest(config.js.DEST_DIR));
+});
+
 gulp.task('minifyScripts', function(){
 	return gulp.src(config.js.app_files)
-		.pipe(concat(config.js.DIST_NAME + '.min.js'))
+		.pipe(concat(`${config.js.DIST_NAME}.min.js`))
 		.pipe(uglify())
 		.pipe(gulp.dest(config.js.DEST_DIR));
 });
@@ -37,8 +43,8 @@ gulp.task('watchSass', ['sass'], function() {
     gulp.watch(config.styles.SOURCE_DIR + '/**/*.scss', ['sass']);
 });
 
-gulp.task('watchScripts', ['minifyScripts'], function(){
-	gulp.watch(config.js.SOURCE_DIR + '/**/*.js', ['minifyScripts']);
+gulp.task('watchScripts', ['minifyScripts', 'concatScripts'], function(){
+	gulp.watch(config.js.SOURCE_DIR + '/**/*.js', ['minifyScripts', 'concatScripts']);
 });
 
 
@@ -46,5 +52,5 @@ gulp.task('watchScripts', ['minifyScripts'], function(){
 * Main gulp tasks
 */
 gulp.task('watch', ['watchSass', 'watchScripts']);
-gulp.task('build', ['minifyScripts', 'sass']);
+gulp.task('build', ['minifyScripts', 'sass', 'concatScripts']);
 gulp.task('default', ['build']);
