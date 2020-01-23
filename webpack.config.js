8fc762976dc92c4b79482fa9a68324ba9d3efc9f
Allen Garvey <allengarvey@hotmail.com>
2020-01-22 20:35:11 -0500

    Rename js_src directory to src

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: 'development',
     entry: [
-            `${__dirname}/js_src/index.js`, 
+            `${__dirname}/src/index.js`, 
             `${__dirname}/sass/style.scss`,
         ],
     output: {
