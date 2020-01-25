0a326e96619c8deb33b4faf669ffb8829053a97d
Allen Garvey <allengarvey@hotmail.com>
2020-01-25 12:50:06 -0500

    Refactor tabs module to make more succinct

--- src/tabs.js
+++ src/tabs.js
@@ -2,24 +2,22 @@
  * Functionality for site navigation to trigger page section tabs
  */
 
-//saves some typing
-var $ = function(selector){ return document.querySelectorAll(selector); };
+// saves some typing
+// binding required to avoid illegal invocation
+// https://stackoverflow.com/questions/10743596/why-are-certain-function-calls-termed-illegal-invocations-in-javascript
+const $ = document.querySelectorAll.bind(document);
 
-var navLinks = $('.nav-list li');
-var pageSections = $('.tab[data-tab]');
+const navLinks = $('.nav-list li');
+const pageSections = $('.tab[data-tab]');
 
-function navLinkClicked(navLink){
-	deselectNavlinks();
-	navLink.classList.add('active');
+function navLinkClicked(navLink, index){
+	navLinks.forEach((navLink, i) => {
+		const action = i === index ? 'add' : 'remove';
+		navLink.classList[action]('active');
+	});
 	const tabSelector = navLink.dataset.tab;
 	hideAllPageSections();
 	document.querySelector('.tab[data-tab="' + tabSelector + '"]').classList.add('active');
-}
-
-function deselectNavlinks(){
-	navLinks.forEach((navLink) => {
-		navLink.classList.remove('active');
-	});
 }
 
 function hideAllPageSections(){
@@ -30,12 +28,12 @@ function hideAllPageSections(){
 
 export function initNavLinks(){
 	//set navLink to be active on click
-	navLinks.forEach((navLink) => {
+	navLinks.forEach((navLink, i) => {
 		navLink.onclick = () => {
-			navLinkClicked(navLink);
+			navLinkClicked(navLink, i);
 		};
 	});
 
 	//select first navLink active by default on page load
-	navLinkClicked(navLinks[0]);
+	navLinkClicked(navLinks[0], 0);
 }
