b9c500a28cbd64fce215d7682b4e0232eb5e4f3d
Allen Garvey <allengarvey@hotmail.com>
2020-01-25 13:04:04 -0500

    Move briefings function into separate module

--- src/game.js
+++ src/game.js
@@ -15,6 +15,7 @@ import mixer from './mixer.js';
 import menu from './menu.js';
 import textOverlay from './text-overlay.js';
 import uiStats from './ui-stats.js';
+import Briefings from './briefings.js';
 
 
 export function start(LEVEL_STATS, AUDIO_STATS, levelIndex, difficultyLevel, savedGame){
@@ -204,7 +205,7 @@ export function start(LEVEL_STATS, AUDIO_STATS, levelIndex, difficultyLevel, sav
 		}
 		//player beat last level, so show congratulations
 		else{
-			showBriefings(briefings, ready, userInfo.levelIndex + 1);
+			Briefings.show(userInfo.levelIndex + 1, displayUserTurnText, enableButtons);
 		}
 	}
 
@@ -539,59 +540,7 @@ export function start(LEVEL_STATS, AUDIO_STATS, levelIndex, difficultyLevel, sav
 		renderer.renderLevel(terrainCanvasContext, levelSpritesheet);
 		renderer.renderInitialGameboard(gameboard, unitCanvasContext);
 	}
-	
 
-	/**
-	 * Displays modal briefings before respective level
-	 * @param briefings - modal to be displayed
-	 * @param ready - span to close modal and begin level
-	 * @param levelIndex - -1 for random setup or otherwise index of level in levelStats
-	 */
-	function showBriefings(briefings, ready, levelIndex) {
-		//If last mission completed, show congratulatory message
-		if(levelIndex > 2)
-		{
-			document.getElementById('level').innerHTML = 'Excellent work! We have reclaimed Capricornus Island. Now that we have taken back our home base, victory will surely be ours. I see a bright future ahead for you. Thanks for all your hard work, soldier!';
-			document.getElementById('ready-btn').innerHTML = 'Return to Main Menu';
-			briefings.style.display = 'block';
-			ready.onclick = function () {
-				briefings.style.display = 'none';
-				menu.displayMainMenu();
-			};
-		}	
-		//Otherwise show briefing for appopriate level
-		else
-		{	
-			if(levelIndex == 0)
-			{
-				var brief = 'Welcome recruit to the legendary Capricornus Squadron. We’re glad to have you with us, as we’ve recently had some setbacks and have fallen on hard times. The enemy have taken over the Gemini Bridges, a vital position in our supply line, and we need to take it back. With your help we should be able to do so.</br></br>When attacking remember that infantry have advantages over planes, planes have advantages over tanks, and tanks have advantages over infantry. Also, infantry will take less damage if they are taking cover in mountains or trees. If you keep these things in mind you should be able to claim victory on the battlefield. Good luck out there soldier!';
-			}
-			
-			else if(levelIndex == 1)
-			{
-				var brief = 'Well done soldier! Now that we have regained control of the Gemini Bridges we should be able to push through some much needed supplies. We can now make way to the port town of Aquarius - an integral location in the war. Currently the enemy have one of their main bases set up there. If we can overtake them, we should be well on our way to flipping the momentum in our favor.</br></br>Remember that only planes will be able to cross the water here. You can use this advantage to keep them out of harm\'s way until they are ready to strike. Alright, its time to move in. Keep up the good work.';
-			}
-
-			else if(levelIndex == 2)
-			{
-				var brief = 'Great going soldier! If you keep this up, you will be moving up the ranks in no time. We now have just one battle left. It is time to reclaim Capricornus Island. If we can take back what is rightfully ours from the enemies standing guard there, we will surely win the war and regain our legendary status as the greatest squadron in the world!</br></br>Just remember that attacking the nearest enemey may not always be the optimal decision. Sometimes it is better to attack an enemy that is further away if it will give you an advantage or just move away and take cover. Be wary of how far an enemy unit is from you and try to get the first strike on a unit.';
-			}
-
-		
-			document.getElementById('level').innerHTML = brief;		
-			document.getElementById('ready-btn').innerHTML = 'READY';
-
-		
-			briefings.style.display = 'block';
-			ready.onclick = function () {
-				briefings.style.display = 'none';
-				displayUserTurnText(function(){
-					enableButtons();
-				});
-			};
-		}
-	}
-	
 
 	/**
 	 * Game initialization stuff below
@@ -615,11 +564,7 @@ export function start(LEVEL_STATS, AUDIO_STATS, levelIndex, difficultyLevel, sav
 
 	//global variables containing game state
 	var gameboard;
-	var userInfo;
-	
-	//Briefings variables
-	var briefings = document.getElementById('briefings');
-	var ready = document.getElementsByClassName('ready')[0];		
+	var userInfo;		
 
 	//setup global variables and render initial gamestate
 	if(savedGame){
@@ -630,7 +575,7 @@ export function start(LEVEL_STATS, AUDIO_STATS, levelIndex, difficultyLevel, sav
 	}
 	else{
 		initializeGame(levelIndex, difficultyLevel, savedGame);
-		showBriefings(briefings, ready, userInfo.levelIndex);
+		Briefings.show(userInfo.levelIndex, displayUserTurnText, enableButtons);
 	}
 
 
