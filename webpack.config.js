1839ef7e163dcc6c18d531ce2784508899d09b32
Allen Garvey <allengarvey@hotmail.com>
2020-10-31 10:29:50 -0400

    Change briefing paper background image to be ignored by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                         loader: MiniCssExtractPlugin.loader,
                     },
                     {
-                        loader: 'css-loader',
+                        loader: 'css-loader?url=false',
                     },
                     {
                         loader: 'sass-loader',
